a3b5b5910ef1e0095112136db0697d865ba63aec
ben thacker <benthac@gmail.com>
2021-05-05 18:43:57 -0400

    first js adds

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert!  Javascript is running!");
+// this creates a function named 'fight'
+var playerName = window.prompt("what is your robot's name?");
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("our robot's name is " + playerName);
+function fight() {
+window.alert("the fight has begun!");
+}
+// fight();
